69f766070ccd6118009d73c7768bb772f176fa60
심준용 <sjy0175@gmail.com>
2024-04-20 11:27:03 +0000

    header & footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,56 @@
-export default async function Footer() {
-    return (
-        <section className="h-[100px] bg-yellow-700">Footer</section>
-    )
+"use client";
+
+import { Button } from "flowbite-react";
+import { useState } from "react";
+
+export default function Footer() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const onPageChange = (page: number) => setCurrentPage(page);
+  return (
+    <section className="flex items-center justify-center gap-4 h-[60px] bg-gray-800 text-white">
+      <button className="flex items-center justify-center p-2 rounded-md bg-transparent border-transparent hover:bg-gray-100 hover:bg-opacity-10 active:border-0">
+        <svg
+          className="w-6 h-6 text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 8 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
+          />
+        </svg>
+        <div className="w-2"></div>
+        <p className="text-base">이전</p>
+      </button>
+      <span className="flex items-center justify-center gap-2 w-12 font-semibold">
+        <p>1</p>
+        <p>/</p>
+        <p>4</p>
+      </span>
+      <button className="flex items-center justify-center p-2 rounded-md bg-transparent border-transparent hover:bg-gray-100 hover:bg-opacity-10 active:border-0">
+        <p className="text-base">다음</p>
+        <div className="w-2"></div>
+        <svg
+          className="w-6 h-6 text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 8 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+          />
+        </svg>
+      </button>
+    </section>
+  );
 }
